aea4f88e8f8f0143e8975996f1d32e4dc6ba4f41
marsondeveloper <marson4ik2019@gmail.com>
2019-09-13 03:22:51 +0400

    93 paginator, постраничный вывод

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import {create} from "react-test-renderer";
 import ProfileStatus from "./ProfileStatus";
 
 describe("ProfileStatus component", () => {
     test("status from props should be in the state ", () => {
-        const component = create(<ProfileStatus status="it-kamasutra.com" />);
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
         const instance = component.getInstance();
         expect(instance.state.status).toBe("it-kamasutra.com");
     });
 
     test("after creation <span> should be displayed", () => {
-        const component = create(<ProfileStatus status="it-kamasutra.com" />);
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
         const root = component.root;
         let span = root.findByType("span");
         expect(span).not.toBeNull();
@@ -18,7 +18,7 @@ describe("ProfileStatus component", () => {
 
 
     test("after creation <span> shouldn't be displayed", () => {
-        const component = create(<ProfileStatus status="it-kamasutra.com" />);
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
         const root = component.root;
 
         expect(() => {
@@ -27,14 +27,14 @@ describe("ProfileStatus component", () => {
     });
 
     test("after creation <span> should contains correct status", () => {
-        const component = create(<ProfileStatus status="it-kamasutra.com" />);
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
         const root = component.root;
         let span = root.findByType("span");
         expect(span.children[0]).toBe("it-kamasutra.com");
     });
 
     test("input should be displayed in editMode instead of span", () => {
-        const component = create(<ProfileStatus status="it-kamasutra.com" />);
+        const component = create(<ProfileStatus status="it-kamasutra.com"/>);
         const root = component.root;
         let span = root.findByType("span");
         span.props.onDoubleClick();
@@ -44,7 +44,7 @@ describe("ProfileStatus component", () => {
 
     test("callback should be called", () => {
         const mockCallback = jest.fn();
-        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus={mockCallback} />);
+        const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus={mockCallback}/>);
         const instance = component.getInstance();
         instance.deactivatedEditMode();
         expect(mockCallback.mock.calls.length).toBe(1);
