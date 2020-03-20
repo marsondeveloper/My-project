fdfb077193854031aa109cc26ca6e078918106a5
удвщкфвщ <marson4ik2019@gmail.com>
2020-03-21 01:46:22 +0200

    Updates

---
+++ precache-manifest.97f53520b4714adee430e1095858569e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9fcaf0a691bc70e0434ea0d4802d223",
+    "url": "//marsondeveloper.github.io/demo-test/index.html"
+  },
+  {
+    "revision": "b62a337035b3d71c3bdd",
+    "url": "//marsondeveloper.github.io/demo-test/static/css/3.0a081a97.chunk.css"
+  },
+  {
+    "revision": "a7a047e4ca15d58d79f8",
+    "url": "//marsondeveloper.github.io/demo-test/static/css/4.bccf235c.chunk.css"
+  },
+  {
+    "revision": "6848de5f5d9ef180523d",
+    "url": "//marsondeveloper.github.io/demo-test/static/css/main.14b90c15.chunk.css"
+  },
+  {
+    "revision": "e07f1328971355dcd6d2",
+    "url": "//marsondeveloper.github.io/demo-test/static/js/2.94f8ad3e.chunk.js"
+  },
+  {
+    "revision": "b62a337035b3d71c3bdd",
+    "url": "//marsondeveloper.github.io/demo-test/static/js/3.2bbda77c.chunk.js"
+  },
+  {
+    "revision": "a7a047e4ca15d58d79f8",
+    "url": "//marsondeveloper.github.io/demo-test/static/js/4.69196364.chunk.js"
+  },
+  {
+    "revision": "6848de5f5d9ef180523d",
+    "url": "//marsondeveloper.github.io/demo-test/static/js/main.bbd6ac66.chunk.js"
+  },
+  {
+    "revision": "37ed4593d9f9c6367fe4",
+    "url": "//marsondeveloper.github.io/demo-test/static/js/runtime~main.da4fedba.js"
+  },
+  {
+    "revision": "be723376487b8fa17548b8fdcdb92ca7",
+    "url": "//marsondeveloper.github.io/demo-test/static/media/preloader.be723376.gif"
+  }
+]);
